65e8e0c62a5014961951914750a71ef0385938d1
ethan <ethanttbui@gmail.com>
2017-07-28 12:23:29 +0800

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
